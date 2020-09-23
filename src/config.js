c855c71255137fe9ad9dbbd9df9b6a8d3bacf7b5
Marco Pestrin <pestrinmarco@gmail.com>
2020-09-23 11:03:38 +0200

    created 2 new components + export key + style pagination + less code for spinner

--- src/config.js
+++ src/config.js
@@ -3,3 +3,9 @@ export const STORE_ID = "ijpxNJLM732vm8AeajMR"
 export const MAIN_LABEL ="CHANGE IN GRID"
 export const ALTERNATIVE_LABEL ="CHANGE IN TABLE"
 export const DATA_KEY = ['id', 'userId', 'title', 'body']
+export const BREAKPOINT_OFFSET = [
+    { value: '10', label: '10' },
+    { value: '15', label: '15' },
+    { value: '20', label: '20' },
+    { value: '25', label: '25' },
+]
